0adfa8ce0c98aa2c43d3ba878aff17d1714e9317
sensha <senshational@gmail.com>
2024-10-10 00:45:13 +0200

    already forgor bump version

--- js/SneshGPT_sidebar.user.js
+++ js/SneshGPT_sidebar.user.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://chatgpt.com/*
 // @grant       none
-// @version     1.0.0
+// @version     1.0.1
 // @author      sensha
 // @downloadURL https://github.com/senshastic/sensha-betterer-todoist/raw/refs/heads/main/js/SneshGPT_sidebar.user.js
 // @updateURL   https://github.com/senshastic/sensha-betterer-todoist/raw/refs/heads/main/js/SneshGPT_sidebar.user.js
